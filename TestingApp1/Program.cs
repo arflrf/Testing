3beb1d1f76c3cfed66ff900834e7c52e8b569bc6
Aaron Forehand <aaron.forehand@okstate.edu>
2021-10-19 13:01:06 -0500

    Initial commit

---
+++ TestingApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TestingApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            int x = 36;
+            int y = 43;
+            int xy = x * y;
+            //Console.WriteLine(xy);
+
+            Console.WriteLine("If " + x + " and " + y + " were multiplied together, it would be " + xy);
+
+            while (x == 36)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Red");
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Orange");
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("yellow");
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Green");
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Blue");
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("Magenta");
+            }
+        }
+    }
+}
